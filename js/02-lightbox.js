41c1a33a2ffcad77ccfa40b2eaf8309996042691
Vladyslav Bozhuk <94122235+escape23@users.noreply.github.com>
2023-09-14 19:37:48 +0200

    +

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+
+list.insertAdjacentHTML("beforeend", createMarkup(galleryItems));
+
+function createMarkup(arr) {
+  return arr
+    .map(
+      ({ preview, description, original }) =>
+        `<a class = "gallery__item js-product-item" href = "${original}">
+        <img src ="${preview}" alt = "${description}" class = 'gallery__image' data-source="${original}"/></a>`
+    )
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
